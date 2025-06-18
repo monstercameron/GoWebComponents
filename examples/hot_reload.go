a7a911c976caff341f62cf6055854bad949ccb31
Earl Cameron <mr.e.cameron@gmail.com>
2025-06-17 22:42:57 -0400

    Enhance hot_reload example by implementing state management for a counter and input field. Introduce GoUseState and GoUseFunc for dynamic updates, improving interactivity and user experience in the component.

--- examples/hot_reload.go
+++ examples/hot_reload.go
@@ -6,12 +6,49 @@
 package examples
 
 import (
+	"fmt"
+
 	. "github.com/monstercameron/GoWebComponents/fiber"
 )
 
 func StateTest() func(Attrs) *Element {
 	return func(props Attrs) *Element {
-		message, _ := GoUseState("Hello, World!")
-		return Div(nil, Text(message()))
+		// Counter state
+		count, setCount := GoUseState(0)
+
+		// Input field state
+		inputValue, setInputValue := GoUseState("")
+
+		// Click handler for counter using GoUseFunc
+		handleClick := GoUseFunc(func(event GoEvent) {
+			event.PreventDefault()
+			currentCount := count()
+			setCount(currentCount + 1)
+		})
+
+		// Change handler for input using GoUseFunc
+		handleChange := GoUseFunc(func(event GoEvent) {
+			newValue := event.GetValue()
+			setInputValue(newValue)
+		})
+
+		return Div(nil,
+			H3(nil, Text("State Test Component")),
+			Div(nil,
+				P(nil, Text("Counter: "), Text(fmt.Sprintf("%d", count()))),
+				Button(Attrs{
+					"onclick": handleClick,
+				}, Text("Click me!")),
+			),
+			Div(nil,
+				P(nil, Text("Input: "), Text(inputValue())),
+				Input(Attrs{
+					"type":        "text",
+					"placeholder": "Enter some text...1",
+					"value":       inputValue(),
+					"onchange":    handleChange,
+				}),
+			),
+		)
 	}
 }
